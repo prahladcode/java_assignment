6d38e8e207a9eef75daaf374ff0c1609ac5a21e6
prahladrathwa62 <145431653+prahladcode@users.noreply.github.com>
2023-09-22 00:04:41 +0530

    Delete Assignment1 directory

--- Assignment1/question1.java
+++
@@ -1,9 +0,0 @@
-public class simpleInterest {
-    public static void main(String[] args){
-        byte a = 90;
-        byte b = 25;
-        byte sum = (byte)(a%b);
-        System.out.println("sum:"+sum);
-
-    }
-}
